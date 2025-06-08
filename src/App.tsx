738fdedc5f348b7a0c6e698e9048eba842a450eb
Yug3ne <eugenekoech9@gmail.com>
2025-06-08 16:01:08 +0300

    dark and light mode implementatrion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "./components/modeToggle"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <div className="flex min-h-svh flex-col items-center justify-center slate">
       <Button>Click me</Button>
+      <ModeToggle/>
     </div>
   )
 }
